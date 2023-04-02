313b72b5b4cdb0c87601e92c4889ab8c883cbe14
KeyframeEditor <andhika.rahmanu@mail.ugm.ac.id>
2023-04-03 05:30:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan6_local/HelpActivity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.pertemuan6_local;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * Help screen that provides a floating action button
+ * to start dialing a phone number.
+ */
+public class HelpActivity extends AppCompatActivity {
+
+    // TAG for the dial logging message.
+    private static final String TAG = HelpActivity.class.getSimpleName();
+
+    /**
+     * Creates the view with a floating action button and click listener.
+     *
+     * @param savedInstanceState Bundle with activity's previously saved state.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = getString(R.string.support_phone);
+                callSupportCenter(phoneNumber);
+            }
+        });
+    }
+
+    /**
+     * Sends an intent to dial the phone number using ACTION_DIAL.
+     *
+     * @param phoneNumber Phone number string
+     */
+    private void callSupportCenter(String phoneNumber) {
+        Intent dialIntent = new Intent(Intent.ACTION_VIEW);
+        dialIntent.setData(Uri.parse("tel:" + phoneNumber));
+        startActivity(dialIntent);
+//        if (dialIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(dialIntent);
+//        } else {
+//            Log.e(TAG, getString(R.string.dial_log_message));
+//        }
+    }
+}
